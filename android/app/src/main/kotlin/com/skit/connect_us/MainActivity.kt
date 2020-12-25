494ed4378d95fe1565c47490b67a49173daaf882
nishantkaushik201296 <nishant.kaushik@finclusion.solutions.com>
2020-12-25 19:42:14 +0530

    Added firebase libs

--- android/app/src/main/kotlin/com/skit/connect_us/MainActivity.kt
+++ android/app/src/main/kotlin/com/skit/connect_us/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.connect_us
+package com.skit.connect_us
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -5,18 +5,19 @@ class SplashScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-          gradient: LinearGradient(
-              colors: [
-            Colors.lightBlue[400],
-            Colors.blue[700],
-          ],
-              stops: [
-            0.0,
-            1.0
-          ],
-              begin: FractionalOffset.topLeft,
-              end: FractionalOffset.bottomRight,
-              tileMode: TileMode.repeated)),
+        gradient: LinearGradient(
+            colors: [
+              Colors.lightBlue[400],
+              Colors.blue[700],
+            ],
+            stops: [
+              0.0,
+              1.0,
+            ],
+            begin: FractionalOffset.topLeft,
+            end: FractionalOffset.bottomRight,
+            tileMode: TileMode.repeated),
+      ),
     );
   }
 }
