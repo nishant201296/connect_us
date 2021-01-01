c898f2dc2e8d5229410f1d9cd7ed375f752ee8d2
nishantkaushik201296 <nishant.kaushik@finclusion.solutions.com>
2021-01-01 10:11:36 +0530

    Added message bubble Ui

--- lib/components/message_bubble.dart
+++ lib/components/message_bubble.dart
@@ -1,14 +1,34 @@
 import 'package:connect_us/models/message_model.dart';
 import 'package:flutter/material.dart';
 
-Container buildMessageBubble(MessageModel message, bool isMe) {
-  return Container(
-    child: Column(
-      mainAxisSize: MainAxisSize.max,
-      crossAxisAlignment: isMe ? CrossAxisAlignment.end : CrossAxisAlignment.end,
-      children: [
-        Text(message.getMessage()),
-      ],
-    ),
+Widget buildMessageBubble(MessageModel message, bool isMe) {
+  return Column(
+    mainAxisSize: MainAxisSize.max,
+    crossAxisAlignment: isMe ? CrossAxisAlignment.end : CrossAxisAlignment.end,
+    children: [
+      Container(
+        margin: isMe ? EdgeInsets.only(left: 50) : EdgeInsets.only(right: 50),
+        child: Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Material(
+            elevation: 10.0,
+            color: isMe ? Colors.lightBlue : Colors.grey,
+            borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(20),
+              bottomRight: Radius.circular(20),
+              topLeft: isMe ? Radius.circular(20) : Radius.zero,
+              topRight: isMe ? Radius.zero : Radius.circular(20),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Text(
+                message.getMessage(),
+                style: TextStyle(color: isMe ? Colors.white : Colors.black, fontSize: 15),
+              ),
+            ),
+          ),
+        ),
+      )
+    ],
   );
 }
